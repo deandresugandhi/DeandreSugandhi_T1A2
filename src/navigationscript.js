4e2e688a4fdca06329a17318a5cb3d1291286914
deandresugandhi <142867392+deandresugandhi@users.noreply.github.com>
2023-09-30 23:38:42 +1000

    Delete src directory

--- src/navigationscript.js
+++
@@ -1,9 +0,0 @@
-const hamburger = document.querySelector(".hamburger");
-const mobilemenu = document.querySelector(".mobilemenu");
-
-hamburger.addEventListener("click", menuClicked);
-
-function menuClicked() {
-    hamburger.classList.toggle("active");
-    mobilemenu.classList.toggle("active");
-}
